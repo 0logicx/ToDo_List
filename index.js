55644362a274e28a9e75aabad34d5ac55e3e7652
0xlogic <iu.markin0977@gmail.com>
2023-06-29 23:47:52 +0300

    startjs

---
+++ index.js
@@ -0,0 +1,18 @@
+// Поиск элементов на странице
+const button__add = document.querySelector('.button__add')
+const input = document.querySelector('.input')
+const add__task = document.querySelector('.add__task')
+const form = document.querySelector('.form')
+
+// Вытаскиваю текст с задачи
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const text_from_input = input.value;
+    
+    console.log('2132132132')
+    
+})
+
